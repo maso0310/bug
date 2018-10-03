3c06fbdca7c16d852188255a00ce8861428c0bb4
maso0310 <balimon@hotmail.com>
2018-10-03 13:59:33 +0800

    'ok'

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def handle_message(event):
         print('csrftoken='+csrf_value)
 
         data = {'csrfmiddlewaretoken':csrf_value}
-        files = {'myfile':open('RIMG0424.jpg','rb')}
+        files = {'myfile':open(path,'rb')}
         headers = {
             'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
             'Cookie':'csrftoken='+csrf_value,
