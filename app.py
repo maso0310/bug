a46248f603308e914b08c7eea66a2c9e261e0d1f
maso0310 <balimon@hotmail.com>
2018-10-04 13:11:17 +0800

    'ok'

--- app.py
+++ app.py
@@ -153,22 +153,22 @@ def handle_message(event):
                 time.sleep(0.1)
                 break
 
-            print('csrftoken='+csrf_value)
-            soup_get = BeautifulSoup(res_get.text,'html.parser')
-            csrf_value = soup_get.find('input')['value']
-            data = {'csrfmiddlewaretoken':csrf_value}
-            files = {'myfile':open('RIMG0424.JPG','rb')}
-            headers = {
-                'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-                'Cookie':'csrftoken='+csrf_value,
-                'Referer':'http://140.113.238.34:8000/'
-            }
+        print('csrftoken='+csrf_value)
+        soup_get = BeautifulSoup(res_get.text,'html.parser')
+        csrf_value = soup_get.find('input')['value']
+        data = {'csrfmiddlewaretoken':csrf_value}
+        files = {'myfile':open('RIMG0424.JPG','rb')}
+        headers = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+            'Cookie':'csrftoken='+csrf_value,
+            'Referer':'http://140.113.238.34:8000/'
+        }
 
-            res_post = requests.post(url,files=files,headers=headers,data=data)
-            soup_post = BeautifulSoup(res_post.text,'html.parser')
-            outcome = soup_post.find('p')
-            bug_number = outcome.text
-            a = '試試看回傳的文字'
+        res_post = requests.post(url,files=files,headers=headers,data=data)
+        soup_post = BeautifulSoup(res_post.text,'html.parser')
+        outcome = soup_post.find('p')
+        bug_number = outcome.text
+        a = '試試看回傳的文字'
             
             print(a)
 
