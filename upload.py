78ae8a3bd40299a34d9e5a59614ceef5447ed8bf
maso0310 <balimon@hotmail.com>
2018-10-09 13:16:58 +0800

    'ok'

--- upload.py
+++ upload.py
@@ -4,19 +4,19 @@ from bs4 import BeautifulSoup
 
 def post_image_to_url(path):
         url = 'http://140.113.238.34:8000/'
-
+'''
         proxies=[
             'http://118.178.124.33:3128',
             'http://139.129.166.68:3128',
             'http://61.143.228.162'
         ]
-
+'''
         headers = {
             'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
             'Referer':'http://140.113.238.34:8000/'
         }
-
-        res_get = requests.get(url,headers=headers,proxies={'http':random.choice(proxies)},timeout=999999999)
+#,proxies={'http':random.choice(proxies)}
+        res_get = requests.get(url,headers=headers,timeout=999999999)
         soup_get = BeautifulSoup(res_get.text,'html.parser')
         csrf_value = soup_get.find('input')['value']
         print('csrftoken='+csrf_value)
