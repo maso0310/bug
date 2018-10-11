19e1f029aaccf83ddac764ba9b801b88ab048cb3
maso0310 <balimon@hotmail.com>
2018-10-11 12:17:26 +0800

    'ok'

--- proxy.py
+++ proxy.py
@@ -3,12 +3,14 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import random
 
-ua = UserAgent() # From here we generate a random user agent
-proxies = [] # Will contain proxies [ip, port]
+ua = UserAgent() # From here we generate a random user agent 從這裡我們生成一個隨機用戶代理
+proxies = [] #Will contain proxies [ip, port]  將包含代理[ip，port]
 
 # Main function
+#主程式
 def main():
-  # Retrieve latest proxies
+  #Retrieve latest proxies
+  #檢視最新的代理
   proxies_req = Request('https://www.sslproxies.org/')
   proxies_req.add_header('User-Agent', ua.random)
   proxies_doc = urlopen(proxies_req).read().decode('utf8')
@@ -16,14 +18,16 @@ def main():
   soup = BeautifulSoup(proxies_doc, 'html.parser')
   proxies_table = soup.find(id='proxylisttable')
 
-  # Save proxies in the array
+  #Save proxies in the array
+  #將代理存在陣列裡面
   for row in proxies_table.tbody.find_all('tr'):
     proxies.append({
       'ip':   row.find_all('td')[0].string,
       'port': row.find_all('td')[1].string
     })
 
-  # Choose a random proxy
+  #Choose a random proxy
+  #選擇一個隨機代理
   proxy_index = random_proxy()
   proxy = proxies[proxy_index]
 
@@ -32,11 +36,13 @@ def main():
     req.set_proxy(proxy['ip'] + ':' + proxy['port'], 'http')
 
     # Every 10 requests, generate a new proxy
-    if n % 10 == 0:
+    #每十次的request產生一個代理
+    if n % 3 == 0:
       proxy_index = random_proxy()
       proxy = proxies[proxy_index]
 
     # Make the call
+    #製造一個呼叫
     try:
       my_ip = urlopen(req).read().decode('utf8')
       print('#' + str(n) + ': ' + my_ip)
@@ -47,6 +53,7 @@ def main():
       proxy = proxies[proxy_index]
 
 # Retrieve a random index proxy (we need the index to delete it if not working)
+#檢索隨機索引代理（如果不工作，我們需要索引刪除它）
 def random_proxy():
   return random.randint(0, len(proxies) - 1)
 
--- upload.py
+++ upload.py
@@ -6,13 +6,11 @@ def post_image_to_url(path):
     url = 'http://140.113.238.34:8000/'
 
     proxies=[
-        'http://97.90.251.228:8080',
-        'http://104.248.208.209:8080',
-        'http://142.93.77.16:8080',
-        'http://68.183.26.106:8080',
-        'http://159.203.58.149:8080',
-        'http://216.167.165.77:8080',
-        'http://209.234.102.87:8080'
+        'http://31.202.124.44',
+        'http://41.193.198.50',
+        'http://118.174.232.147',
+        'http://94.232.55.98',
+        'http://118.174.232.147'
     ]
 
     res_get = requests.get(url,proxies={"http":random.choice(proxies)})
@@ -21,7 +19,7 @@ def post_image_to_url(path):
     print('csrftoken='+csrf_value)
 
     data = {'csrfmiddlewaretoken':csrf_value}
-    files = {'myfile':open("397892.jpg",'rb')}
+    files = {'myfile':open(path,'rb')}
 
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
@@ -29,7 +27,7 @@ def post_image_to_url(path):
         'Cookie':'csrftoken='+csrf_value
     }
 
-    res_post = requests.post(url,files=files,headers=headers,data=data,proxies={"http":random.choice(proxies)})
+    res_post = requests.post(url,files=files,headers=headers,data=data,proxies={"http":random.choice(proxies)},timeout=3600)
     print(res_post)
     soup_post = BeautifulSoup(res_post.text,'html.parser')
     outcome = soup_post.find_all('p')
