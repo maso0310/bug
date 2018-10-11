fa9a7c7f42dfe6b7d240afe6c3d15fa355a935aa
maso0310 <balimon@hotmail.com>
2018-10-11 16:46:39 +0800

    'ok'

--- shrottest.py
+++ shrottest.py
@@ -1,6 +1,6 @@
 import requests
 
-file = {'uploadFile': open('397892.jpg', 'rb')}
+file = {'uploadFile': open('C:/bug/397892.jpg', 'rb')}
 print(file)
 r = requests.post('http://pythonscraping.com/pages/files/processing2.php', files=file)
 print(r.text)
