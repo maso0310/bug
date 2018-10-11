820af3dcf49bba57a826d2b39d719bdb85dcb956
maso0310 <balimon@hotmail.com>
2018-10-11 14:02:12 +0800

    'ok'

--- upload.py
+++ upload.py
@@ -3,26 +3,26 @@ import random
 from bs4 import BeautifulSoup
 
 def post_image_to_url(path):
-    url = 'http://140.113.238.34:8000/'
+    url = 'http://pythonscraping.com/files/form2.html'
 
-    res_get = requests.get(url,proxies={"https":'https://ricebug.herokuapp.com'})
-    print(res_get)
-    print(res_get.text)
-    soup_get = BeautifulSoup(res_get.text,'html.parser')
-    csrf_value = soup_get.find('input')['value']
-    print('csrftoken='+csrf_value)
+    #res_get = requests.get(url)
+    #print(res_get)
+    #print(res_get.text)
+    #soup_get = BeautifulSoup(res_get.text,'html.parser')
+    #csrf_value = soup_get.find('input')['value']
+    #print('csrftoken='+csrf_value)
 
-    data = {'csrfmiddlewaretoken':csrf_value} 
+    #data = {'csrfmiddlewaretoken':csrf_value} 
     files = {'myfile':open(path,'rb')}
 
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
         'Referer':'http://140.113.238.34:8000/',
-        'host':'ricebug.herokuapp.com',
-        'Cookie':'csrftoken='+csrf_value
+        'host':'ricebug.herokuapp.com'
+    #    'Cookie':'csrftoken='+csrf_value
     }
 
-    res_post = requests.post(url,files=files,headers=headers,data=data,proxies={"http":'http://203.104.146.152'},timeout=3600)
+    res_post = requests.post(url,files=files,headers=headers,timeout=3600)
     print(res_post)
     soup_post = BeautifulSoup(res_post.text,'html.parser')
     outcome = soup_post.find_all('p')
