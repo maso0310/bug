ab3a46cc32acf837b7aed01cd94756eb72a1b723
maso0310 <balimon@hotmail.com>
2018-10-11 10:37:59 +0800

    'ok'

--- upload.py
+++ upload.py
@@ -5,31 +5,32 @@ from bs4 import BeautifulSoup
 def post_image_to_url(path):
     url = 'http://140.113.238.34:8000/'
 
-    res_get = requests.get(url)
+    proxies=[
+        'None'
+    ]
+
+
+    res_get = requests.get(url,proxies={'http':random.choice(proxies)})
     soup_get = BeautifulSoup(res_get.text,'html.parser')
     csrf_value = soup_get.find('input')['value']
     print('csrftoken='+csrf_value)
-
-    data = {'csrfmiddlewaretoken':csrf_value}
-    files = {'myfile':open("397892.jpg",'rb')}
+    print(res_get)
 
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
         'Referer':'http://140.113.238.34:8000/',
-        'Cookie':'csrftoken='+csrf_value
+        'Cookie':'csrftoken='+csrf_value,
+        "Connection": "close"
     }
 
-    proxies=[
-        'http://112.25.41.136:80',
-        'http://61.143.228.162',
-        'http://127.0.0.1:9743'
-    ]
+    data = {'csrfmiddlewaretoken':csrf_value}
+    files = {'myfile':open(path,'rb')}
 
-    res_post = requests.post(url,files=files,headers=headers,data=data)
+    res_post = requests.post(url,files=files,proxies={'http':random.choice(proxies)},headers=headers,data=data,timeout=1200)
     print(res_post)
     soup_post = BeautifulSoup(res_post.text,'html.parser')
     outcome = soup_post.find_all('p')
     print(outcome)
     bug_number = outcome[1].text
-    print(bug_number[9:10])
+    print(bug_number)
     return bug_number[9:10]
