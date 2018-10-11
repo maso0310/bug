6f5100911d0e0f166538325defa32614fefc7ee0
maso0310 <balimon@hotmail.com>
2018-10-11 11:52:20 +0800

    'ok'

--- upload.py
+++ upload.py
@@ -15,29 +15,27 @@ def post_image_to_url(path):
         'http://209.234.102.87:8080'
     ]
 
-
     res_get = requests.get(url)
-    print(res_get)
     soup_get = BeautifulSoup(res_get.text,'html.parser')
     csrf_value = soup_get.find('input')['value']
     print('csrftoken='+csrf_value)
 
+    data = {'csrfmiddlewaretoken':csrf_value}
+    files = {'myfile':open("397892.jpg",'rb')}
 
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
         'Referer':'http://140.113.238.34:8000/',
-        'Cookie':'csrftoken='+csrf_value,
-        "Connection": "close"
+        'Cookie':'csrftoken='+csrf_value
     }
 
-    data = {'csrfmiddlewaretoken':csrf_value}
-    files = {'myfile':open(path,'rb')}
 
-    res_post = requests.post(url,files=files,proxies={'http':random.choice(proxies)},headers=headers,data=data,timeout=1200)
+
+    res_post = requests.post(url,files=files,headers=headers,data=data)
     print(res_post)
     soup_post = BeautifulSoup(res_post.text,'html.parser')
     outcome = soup_post.find_all('p')
     print(outcome)
     bug_number = outcome[1].text
-    print(bug_number)
+    print(bug_number[9:10])
     return bug_number[9:10]
--- utils.py
+++ utils.py
@@ -4,11 +4,21 @@ from bs4 import BeautifulSoup
   
 url = 'http://140.113.238.34:8000/'
 
+proxies=[
+    'http://97.90.251.228:8080',
+    'http://104.248.208.209:8080',
+    'http://142.93.77.16:8080',
+    'http://68.183.26.106:8080',
+    'http://159.203.58.149:8080',
+    'http://216.167.165.77:8080',
+    'http://209.234.102.87:8080'
+]
+
 res_get = requests.get(url)
 soup_get = BeautifulSoup(res_get.text,'html.parser')
 csrf_value = soup_get.find('input')['value']
 print('csrftoken='+csrf_value)
-'''
+
 data = {'csrfmiddlewaretoken':csrf_value}
 files = {'myfile':open("397892.jpg",'rb')}
 
@@ -18,11 +28,7 @@ headers = {
     'Cookie':'csrftoken='+csrf_value
 }
 
-proxies=[
-    'http://112.25.41.136:80',
-    'http://61.143.228.162',
-    'http://127.0.0.1:9743'
-]
+
 
 res_post = requests.post(url,files=files,headers=headers,data=data)
 print(res_post)
@@ -31,4 +37,3 @@ outcome = soup_post.find_all('p')
 print(outcome)
 bug_number = outcome[1].text
 print(bug_number[9:10])
-'''
