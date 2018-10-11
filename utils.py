49a283d265f401cc3a0672953b8466ab49605253
maso0310 <balimon@hotmail.com>
2018-10-11 13:50:07 +0800

    'ok'

--- utils.py
+++ utils.py
@@ -8,16 +8,16 @@ url = 'http://pythonscraping.com/files/form2.html/'
 res_get = requests.get(url,proxies={"https":'https://203.104.146.152'})
 print(res_get)
 soup_get = BeautifulSoup(res_get.text,'html.parser')
-csrf_value = soup_get.find('input')['value']
-print('csrftoken='+csrf_value)
+#csrf_value = soup_get.find('input')['value']
+#print('csrftoken='+csrf_value)
 
-data = {'csrfmiddlewaretoken':csrf_value}
+#data = {'csrfmiddlewaretoken':csrf_value}
 files = {'myfile':open("397892.jpg",'rb')}
 
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-    'Referer':'http://140.113.238.34:8000/',
-    'Cookie':'csrftoken='+csrf_value
+    'Referer':'http://140.113.238.34:8000/'
+#    'Cookie':'csrftoken='+csrf_value
 }
 
 res_post = requests.post(url,files=files,headers=headers,data=data,proxies={"https":'https://203.104.146.152'})
