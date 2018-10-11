79eb04c12d1e148ce635300995a0d55a7befdc51
maso0310 <balimon@hotmail.com>
2018-10-11 12:35:00 +0800

    'ok'

--- upload.py
+++ upload.py
@@ -11,7 +11,7 @@ def post_image_to_url(path):
         'http://94.232.55.98'
     ]
 
-    res_get = requests.get(url)
+    res_get = requests.get(url,verify=false)
     print(res_get)
     soup_get = BeautifulSoup(res_get.text,'html.parser')
     csrf_value = soup_get.find('input')['value']
@@ -23,10 +23,11 @@ def post_image_to_url(path):
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
         'Referer':'http://140.113.238.34:8000/',
+        'host':'ricebug.herokuapp.com',
         'Cookie':'csrftoken='+csrf_value
     }
 
-    res_post = requests.post(url,files=files,headers=headers,data=data,proxies={"http":random.choice(proxies)},timeout=3600)
+    res_post = requests.post(url,files=files,headers=headers,data=data,proxies={"http":random.choice(proxies)},timeout=3600,verify=false)
     print(res_post)
     soup_post = BeautifulSoup(res_post.text,'html.parser')
     outcome = soup_post.find_all('p')
